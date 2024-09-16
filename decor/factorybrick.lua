5e362812925a357abc9cf836df8680387cf1d607
mazes 80 <1608580+mazes-80@users.noreply.github.com>
2024-09-17 00:33:04 +0200

    Stairs: do not preserve stone group

--- decor/factorybrick.lua
+++ decor/factorybrick.lua
@@ -6,10 +6,10 @@ minetest.register_node("factory:factory_brick", {
 	is_ground_content = true,
 	groups = {cracky=3, stone=1}
 })
-if minetest.global_exists("stairs") then
+if minetest.get_modpath("stairs") then
 	stairs.register_stair_and_slab("factory_brick",
 		"factory:factory_brick",
-		{cracky=3, stone=1},
+		{cracky=3},
 		{"factory_brick.png"},
 		S("@1 Slab", S("Factory Brick"))
 	)
@@ -19,7 +19,7 @@ if minetest.get_modpath("moreblocks") then
 	stairsplus:register_all("factory", "factory_brick", "factory:factory_brick", {
 		description = S("Factory Brick"),
 		tiles = {"factory_brick.png"},
-		groups = {cracky=3, stone=1},
+		groups = {cracky=3},
 	})
 end
 -- vim: et:ai:sw=2:ts=2:fdm=indent:syntax=lua
